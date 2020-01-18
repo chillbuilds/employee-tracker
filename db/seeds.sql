c02b5c1e6df96f1503eec9b760d17b09378d74f7
heliumface770 <heliumface770@gmail.com>
2020-01-18 13:50:01 -0700

    seeds created

---
+++ db/seeds.sql
@@ -0,0 +1,16 @@
+  
+INSERT INTO departments (name) VALUES ('Couch Testing');
+INSERT INTO departments (name) VALUES ('Snack Eating');
+INSERT INTO departments (name) VALUES ('Space Force');
+
+INSERT INTO roles (title, salary, department_id) VALUES ('Stain Removal Expert', 45000, 1);
+INSERT INTO roles (title, salary, department_id) VALUES ('Pillow Dude', 75000, 1);
+INSERT INTO roles (title, salary, department_id) VALUES ('Cusion King', 62000, 1);
+
+INSERT INTO roles (title, salary, department_id) VALUES ('Cheese Melter', 40000, 2);
+INSERT INTO roles (title, salary, department_id) VALUES ('Dip Wrangler', 55000, 2);
+INSERT INTO roles (title, salary, department_id) VALUES ('EMT', 70000, 2);
+
+INSERT INTO roles (title, salary, department_id) VALUES ('Freeze Dried Ice Cream Truck Driver', 70000, 3);
+INSERT INTO roles (title, salary, department_id) VALUES ('Space Janitor', 60000);
+INSERT INTO roles (title, salary, department_id) VALUES ('Guy That does *pew* *pew* sounds', 95000, 3);
